ce39c1cd5a9b2f406125398c69da4ab1b965d646
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-10-16 00:39:58 +0100

    updated

--- RxSwift/RxSwift/SplashView.swift
+++ RxSwift/RxSwift/SplashView.swift
@@ -13,7 +13,7 @@ struct SplashView: View {
     
     var body: some View {
         
-        let customColor = Color(hex: 0xFF460A)
+        let customColor = Color(hex: 0xFF4B3A)
         
         ZStack{
             if self.isActive{
@@ -21,21 +21,56 @@ struct SplashView: View {
             } else {
                 
                 customColor.edgesIgnoringSafeArea(.all)
-                VStack {
+                VStack(alignment: .leading) {
                     ZStack{
                         Image(uiImage: UIImage(named: "circle")!)
                             .resizable()
                             .aspectRatio(CGSize(width: 60, height: 60), contentMode: .fit)
+                            .position(x: 150, y: 50)
                             .frame(width: 60, height: 60)
-                            .position(x: 50, y: 50)
                         Image(uiImage: UIImage(named: "appLogo")!)
                             .resizable()
                             .aspectRatio(CGSize(width: 50, height: 50), contentMode: .fit)
+                            .position(x: 150, y: 50)
                             .frame(width: 50, height: 50)
-                            .position(x: 50, y: 50)
+                        
                     }
-        
+                    .padding(.bottom, 40)
+                    Text("Food for \nEveryone")
+                        .position(x: 230, y: 50)
+                        .lineLimit(2)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .font(.system(size: 50))
+                    HStack {
+                        Spacer()
+                        Image(uiImage: UIImage(named: "iconLogo2")!)
+                            .resizable()
+                            .frame(width: 340, height: 430)
+                            .offset(x: 50)
+                            .zIndex(1)
+                        Image(uiImage: UIImage(named: "iconLogo1")!)
+                            .resizable()
+                            .frame(width: 220, height: 290)
+                            .offset(x: -70)
+                    }
+                    .padding(.bottom, 30)
+                    Button(action: {
+                        //("btn tapped")
+                    }) {
+                       Text("Get started")
+                            .font(.system(size: 20, weight: .heavy))
+                            .foregroundColor(.orange)
+                            .padding(.horizontal, 20)
+                            .frame(width: 300, height: 50)
+                            .background(Color.white)
+                            .cornerRadius(25)
+                            .position(x: 280)
+                            .frame(width: 300, height: 50)
+                            .controlSize(.large)
+                    }
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
                 
             }
             

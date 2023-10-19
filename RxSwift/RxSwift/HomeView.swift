70442932555ac33fe8a0e21faa0da52c212e1c5d
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-10-19 21:57:43 +0100

    updated

--- RxSwift/RxSwift/HomeView.swift
+++ RxSwift/RxSwift/HomeView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct HomeView: View {
-    
-    @State var presentSideMenu = false
-    
+    @State var presentSidemenu = false
     var body: some View {
         VStack{
             HStack{
                 Button{
-                    presentSideMenu.toggle()
+                    presentSidemenu.toggle()
                 } label: {
                     Image(uiImage: UIImage(systemName: "text.alignleft")!)
                         .resizable()
@@ -31,16 +29,43 @@ struct HomeView: View {
         .padding(.horizontal, 24)
     }
 }
+
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
 }
 
-struct SideMenu: View {
+struct ContentView: View {
+    @State var presentSidemenu = false
+    var body: some View {
+        NavigationView {
+            MenuContents()
+                .toolbar {
+                    ToolbarItem(placement: .navigation) {
+                        Button(action: {
+                            withAnimation {
+                                presentSidemenu.toggle()
+                            }
+                        }) {
+                            Image(systemName: "text.alignleft")
+                        }
+                    }
+                }
+                .navigationTitle("main content")
+            if presentSidemenu {
+                SideMenuView(presentSidemenu: $presentSidemenu)
+                    .frame(width: 250)
+            }
+        }
+    }
+}
+
+struct SideMenuView: View {
+    @Binding var presentSidemenu: Bool
     var body: some View {
         List {
-            NavigationLink("Home", destination: HomeView())
+            NavigationLink("Home", destination: Text("Homeview"))
             NavigationLink("Profile", destination: Text("Profile"))
             NavigationLink("Orders", destination: Text("Orders"))
             NavigationLink("Offer and Promo", destination: Text("Offer and Promo"))
@@ -48,16 +73,26 @@ struct SideMenu: View {
             NavigationLink("Security", destination: Text("Security"))
         }
         .listStyle(SidebarListStyle())
+        .toolbar {
+            ToolbarItem(placement: .navigation) {
+                Button(action: {
+                    withAnimation {
+                        presentSidemenu.toggle()
+                    }
+                }) {
+                    Image(systemName: "text.alignleft")
+                }
+            }
+        }
     }
 }
 
 struct MenuContents: View {
     var body: some View {
-        VStack {
-            
-        }
+        Text("Main content goes here")
     }
 }
+
 
 
 

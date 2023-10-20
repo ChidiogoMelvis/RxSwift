6782222789e1149933d97f81103dae58d7f15abf
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-10-20 01:09:31 +0100

    updated

--- RxSwift/RxSwift/UserViews/LoginView.swift
+++ RxSwift/RxSwift/UserViews/LoginView.swift
@@ -11,46 +11,51 @@ struct LoginView: View {
     
     @State var email: String = ""
     @State var password: String = ""
-    
+    @State var isPresentNextScreen = false
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Email address")
-                .foregroundColor(Color.gray)
-                .padding(.horizontal, 16)
-                .padding(.vertical, 0)
-            ZStack(alignment: .bottom) {
-                Rectangle()
-                    .frame(height: 1)
-                    .foregroundColor(Color.black)
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Email address")
+                    .foregroundColor(Color.gray)
                     .padding(.horizontal, 16)
-                CustomTextField(email: $email, placeholder: "Enter email")
-            }
-            .padding(.bottom, 50)
-            Text("Password")
-                .foregroundColor(Color.gray)
-                .padding(.horizontal, 16)
-                .padding(.bottom, 5)
-            ZStack(alignment: .bottom) {
-                Rectangle()
-                    .frame(height: 1)
-                    .foregroundColor(Color.black)
+                    .padding(.vertical, 0)
+                ZStack(alignment: .bottom) {
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(Color.black)
+                        .padding(.horizontal, 16)
+                    CustomTextField(email: $email, placeholder: "Enter email")
+                }
+                .padding(.bottom, 50)
+                Text("Password")
+                    .foregroundColor(Color.gray)
                     .padding(.horizontal, 16)
-                SecureCustomTextField(text: $password, placeholder: "Enter password")
-            }
-            Spacer().frame(height: 220)
-            Button(action: {
-                //("btn tapped")
-            }) {
-                Text("Login")
-                    .font(.system(size: 20, weight: .medium))
-                    .foregroundColor(Color.white)
-                    .padding(.horizontal, 20)
-                    .frame(width: 300, height: 50)
-                    .background(Color(hex: 0xFA4A0C))
-                    .cornerRadius(25)
-                    .position(x: 190)
-                    .frame(width: 300, height: 50)
-                    .controlSize(.large)
+                    .padding(.bottom, 5)
+                ZStack(alignment: .bottom) {
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(Color.black)
+                        .padding(.horizontal, 16)
+                    SecureCustomTextField(text: $password, placeholder: "Enter password")
+                }
+                Spacer().frame(height: 150)
+                Button(action: {
+                    isPresentNextScreen.toggle()
+                }) {
+                    Text("Login")
+                        .font(.system(size: 20, weight: .medium))
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal, 20)
+                        .frame(width: 300, height: 50)
+                        .background(Color(hex: 0xFA4A0C))
+                        .cornerRadius(25)
+                        .position(x: 190)
+                        .frame(width: 300, height: 50)
+                        .controlSize(.large)
+                }
+                .sheet(isPresented: $isPresentNextScreen) {
+                    HomeView()
+                }
             }
         }
     }

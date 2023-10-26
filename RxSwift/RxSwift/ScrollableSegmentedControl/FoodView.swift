42b43e2cf7880d868ecde1eb88e352d74d539148
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-10-26 06:14:02 +0100

    updated

--- RxSwift/RxSwift/ScrollableSegmentedControl/FoodView.swift
+++ RxSwift/RxSwift/ScrollableSegmentedControl/FoodView.swift
@@ -8,27 +8,32 @@
 import SwiftUI
 
 struct FoodView: View {
-    let imageNames = ["food1", "food2", "food3", "food4"]
-    
+    let imageNames = ["food1","food2","food3","food4"]
+
+
+
     var body: some View {
         Color(hex: 0xEDEDED)
             .edgesIgnoringSafeArea(.all)
             .overlay (
-        ScrollView(.horizontal) {
-            LazyHGrid(rows:
-                        [GridItem(.flexible()),
-                        ], spacing: -1) {
-                ForEach(imageNames, id: \.self) { imageName in
-                    Image(imageName)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 220, height: 321)
+                ScrollView(.horizontal) {
+                    HStack(spacing: -50) {
+                        ForEach(imageNames, id: \.self) { imageName in
+                            Image(imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 220, height: 321)
+                        }
+                    }
+                    //.padding(.horizontal, -10)
+                    .padding(.top, 50)
                 }
-            }
-                        .padding(.top, 50)
-        })
+
+
+            )
     }
 }
+
 
 struct FoodView_Previews: PreviewProvider {
     static var previews: some View {
